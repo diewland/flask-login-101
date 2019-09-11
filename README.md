e34bd44a8fb79bb093fd2e78c77302787f37df43
yuwat <yuwat@mfec.co.th>
2019-09-11 18:15:36 +0700

    init project

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # flask-login-101
+
+* https://github.com/maxcountryman/flask-login
+* https://github.com/ihoegen/Flask-Login-App-Tutorial
+* https://docs.python.org/3/library/hashlib.html
+* https://en.wikipedia.org/wiki/Secure_Hash_Algorithms
---
+++ app.py
@@ -0,0 +1,84 @@
+import flask
+import flask_login
+import hashlib
+
+app = flask.Flask(__name__)
+app.secret_key = 'super secret string'  # Change this!
+
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+
+def hash_text(text):
+    return hashlib.sha3_512(text.encode()).hexdigest()
+
+# Our mock database.
+users = {'admin@admin.com': {'password': hash_text('admin')}}
+print(users)
+
+class User(flask_login.UserMixin):
+    pass
+
+@login_manager.user_loader
+def user_loader(email):
+    if email not in users:
+        return
+
+    user = User()
+    user.id = email
+    return user
+
+@login_manager.request_loader
+def request_loader(request):
+    email = request.form.get('email')
+    if email not in users:
+        return
+
+    user = User()
+    user.id = email
+
+    # DO NOT ever store passwords in plaintext and always compare password
+    # hashes using constant-time comparison!
+    user.is_authenticated = hash_text(request.form['password']) == users[email]['password']
+
+    return user
+
+@app.route('/', methods=['GET'])
+def index():
+    return flask.redirect('/login')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if flask.request.method == 'GET':
+        return '''
+               <form action='login' method='POST'>
+                <input type='text' name='email' id='email' placeholder='email'/>
+                <input type='password' name='password' id='password' placeholder='password'/>
+                <input type='submit' name='submit'/>
+               </form>
+               '''
+
+    email = flask.request.form['email']
+    if hash_text(flask.request.form['password']) == users[email]['password']:
+        user = User()
+        user.id = email
+        flask_login.login_user(user)
+        return flask.redirect(flask.url_for('protected'))
+
+    return 'Bad login'
+
+@app.route('/protected')
+@flask_login.login_required
+def protected():
+    return 'Logged in as: ' + flask_login.current_user.id
+
+@app.route('/logout')
+def logout():
+    flask_login.logout_user()
+    return 'Logged out'
+
+@login_manager.unauthorized_handler
+def unauthorized_handler():
+    return 'Unauthorized'
+
+if __name__ == '__main__':
+    app.run(debug=True)
